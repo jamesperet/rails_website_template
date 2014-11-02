8728b44be8bb867f4f5ff7253721adb9f103a688
jamesperet <james.peret@gmail.com>
2014-11-01 23:42:28 -0200

    reset password email translation and fixes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,8 +27,11 @@ RailsWebsiteTemplate::Application.configure do
   # number of complex assets.
   config.assets.debug = true
   
-  #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+  
   #config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
   
   config.action_mailer.smtp_settings = {
       :address   => "smtp.mandrillapp.com",
