22c3e4bd0d30f7e20ebbee7fa40251c30f8b90ca
jamesperet <james.peret@gmail.com>
2015-01-10 14:09:00 -0200

    carrierwave cache dir fix for heroku

--- app/uploaders/cover_uploader.rb
+++ app/uploaders/cover_uploader.rb
@@ -39,7 +39,7 @@ class CoverUploader < CarrierWave::Uploader::Base
   end
   
   def cache_dir
-    " ./tmp/uploads/#{mounted_as}/"
+    "#{Rails.root}/tmp/uploads/#{mounted_as}/"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -36,7 +36,7 @@ class FileUploader < CarrierWave::Uploader::Base
   end
   
   def cache_dir
-    " ./tmp/uploads/#{mounted_as}/"
+    "#{Rails.root}/tmp/uploads/#{mounted_as}/"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
